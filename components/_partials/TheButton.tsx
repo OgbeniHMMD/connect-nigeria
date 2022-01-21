9e329b80464e266918edde6abc142cf14d49b68a
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-21 06:02:19 +0100

    Improved Button Component

--- components/_partials/TheButton.tsx
+++ components/_partials/TheButton.tsx
@@ -1,7 +1,17 @@
-export default function TheButton({ children }) {
+export default function TheButton({ children, primary, secondary }: Props) {
   return (
-    <button className="bg-primary rounded font-bold text-sm text-white py-2 px-8 uppercase md:text-base lg:py-4 lg:px-12 hover:underline">
+    <button
+      className={`rounded font-bold text-sm text-white py-2 px-8 uppercase md:text-base lg:py-4 lg:px-12 hover:underline ${
+        secondary ? "bg-secondary" : "bg-primary"
+      }`}
+    >
       {children}
     </button>
   );
+}
+
+interface Props {
+  children: any;
+  primary?: boolean;
+  secondary?: boolean;
 }
